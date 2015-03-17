876cb679707d5acc6d4230210e117ccc6d52cdfa
Marcin Swiderek <svdr23@gmail.com>
2015-03-17 15:53:01 +0100

    Zadanie z to do list

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-var Buttons=document.querySelectorAll('.btn--delete');
+var myList=document.getElementById('my-todo'),
+	myForm=document.getElementById('my-form'),
+	myInput=document.getElementById('new_item');
 
-for(var i=0;i<Buttons.length;i++) {
-	Buttons[i].addEventListener('click', function(event) {
-		event.target.parentNode.remove()
-	});	
+myForm.addEventListener('click',function(event) {
 	
-}
+	event.preventDefault();
+	//create list item
+	var listItem=document.createElement("li"),
+		listItemText=document.createTextNode(myInput.value);
+	//append text to list item
+	listItem.appendChild(listItemText);
+	
+	//append list item to menu 
+	myList.appendChild(listItem);
+	//clear inputValue
+	
+	myInput.value='';
+});
